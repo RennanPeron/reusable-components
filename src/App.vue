6e3039a6ab854b69438c9fa7b6b08456440e0300
RennanPeron <rennanperon01@gmail.com>
2023-08-14 15:59:29 -0300

    base layout

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,22 @@
 <template>
-  <RouterView />
+  <aside class="col-auto bg-[#FAFBFD] pl-3 lg:pl-10 xl:pl-12 pt-12">
+    <header class="fixed">
+      <h1 class="mb-28"><span class="text-[#F7542E]">Dev</span>challengs.io</h1>
+
+      <nav>
+        <ul class="list-none grid gap-8">
+          <li>Colors</li>
+          <li>Typography</li>
+          <li>Spaces</li>
+          <li>Buttons</li>
+          <li>Inputs</li>
+          <li>Grid</li>
+        </ul>
+      </nav>
+    </header>
+  </aside>
+
+  <RouterView class="col-span-4 px-20 py-14" />
 </template>
 
 <script setup>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <main>
-    <h1>Starting!</h1>
+    <h1>Buttons</h1>
   </main>
 </template>
 
