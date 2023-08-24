8eb4aee73356bc2b45a9100948d59f831822372f
RennanPeron <rennanperon01@gmail.com>
2023-08-24 17:34:25 -0300

    mobile layout

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,10 @@
 <template>
-  <aside class="col-auto bg-[#FAFBFD] pl-3 lg:pl-10 xl:pl-12 pt-12">
-    <header class="fixed">
-      <h1 class="mb-28"><span class="text-[#F7542E]">Dev</span>challengs.io</h1>
+  <app-header />
 
-      <nav>
-        <ul class="list-none grid gap-8">
-          <li>Colors</li>
-          <li>Typography</li>
-          <li>Spaces</li>
-          <li>Buttons</li>
-          <li>Inputs</li>
-          <li>Grid</li>
-        </ul>
-      </nav>
-    </header>
-  </aside>
-
-  <RouterView class="col-span-4 px-20 py-14" />
+  <RouterView class="col-span-4 px-10 py-20 lg:px-20 lg:py-14 lg:ml-44" />
 </template>
 
 <script setup>
+import AppHeader from '@/components/AppHeader.vue'
 import { RouterView } from 'vue-router'
 </script>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="w-full px-10 pt-4 absolute text-center inline lg:hidden ">
+        <button @click.prevent="modal.toggleModal" class="float-left"><span class="material-symbols-outlined">
+                menu
+            </span></button>
+        <h1>
+            <span class="text-[#F7542E]">Dev</span>challengs.io
+        </h1>
+    </div>
+
+    <header :class="{ 'hidden': !modal.isOpen }" class="lg:block fixed h-screen col-auto bg-[#FAFBFD] pl-10 px-12 pt-12">
+        <h1 class="mb-28 hidden lg:block"><span class="text-[#F7542E]">Dev</span>challengs.io</h1>
+
+        <button @click.prevent="modal.toggleModal"
+            class="mb-10 block lg:hidden rounded-full bg-white w-10 h-10 flex justify-center items-center">
+            <span class="material-symbols-outlined">
+                close
+            </span>
+        </button>
+
+        <nav>
+            <ul class="list-none grid gap-8">
+                <li>Colors</li>
+                <li>Typography</li>
+                <li>Spaces</li>
+                <li>Buttons</li>
+                <li>Inputs</li>
+                <li>Grid</li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script setup>
+import { useModalStore } from "@/stores/modal"
+
+const modal = useModalStore()
+</script>
